9677e2e70068442c27561e855e6293fe157bab44
TheNightman <joyod3@gmail.com>
2016-12-02 15:09:05 -0500

    Fixed sender name in client

--- cirkit-core/client.js
+++ cirkit-core/client.js
@@ -2,7 +2,7 @@ var request = require('request');
 
 exports.send = function (msg, sendTo) {
   request.post('http://' +sendTo +':6969/',
-      { json: {msg: msg, sender: "gs6"}},
+      { json: {msg: msg, sender: "Cirkit Server"}},
       function (error, response, body) {
         if (!error && response.statusCode == 200) {
           console.log(body)
